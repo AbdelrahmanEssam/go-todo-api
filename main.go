aa79b7c81da633ce1fde3566a4cbcb20edf26f73
AbdelrahmanEssam <abdelrahman.en@gmail.com>
2025-05-11 18:01:45 +0300

    Implemet POST /todos

--- main.go
+++ main.go
@@ -15,14 +15,14 @@ type Todo struct {
 
 var todos = []Todo{}
 
-//var idCounter = 1
+var idCounter = 1
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Todo API is running")
 	})
 
-	http.HandleFunc("/todos", getTodos)
+	http.HandleFunc("/todos", todosHandler)
 
 	fmt.Println("Server is listening on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -36,4 +36,32 @@ func getTodos(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(todos)
+}
+
+func todosHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodGet:
+		getTodos(w, r)
+	case http.MethodPost:
+		createTodo(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func createTodo(w http.ResponseWriter, r *http.Request) {
+	var t Todo
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	t.ID = idCounter
+	idCounter++
+	todos = append(todos, t)
+
+	w.Header().Set("Cintent-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(t)
 }
